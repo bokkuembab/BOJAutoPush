0dfa717a0598591560f4b2afa7e8dce0cc30943d
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-10-02 21:44:23 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.34 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,13 @@
+def binary_trans(s):
+    ct = s.count('0')
+    tr = bin(len(s.replace('0', '')))
+    return ct, str(tr[2:])
+
+def solution(s):
+    ans = [0, 0]
+    while s != '1':
+        ct, s = binary_trans(s)
+        ans[0] += 1  
+        ans[1] += ct
+
+    return ans

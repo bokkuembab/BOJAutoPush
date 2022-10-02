29cdd995393fd31f6a2d846bb0fbb1e56b4ec52c
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-10-02 21:44:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    arr = list(s.split(' '))
+    ans = []
+    
+    for i in range(len(arr)):
+        ans.append(arr[i][:1].upper() + arr[i][1:].lower())
+    ans = ' '.join(ans)
+    
+    return ans

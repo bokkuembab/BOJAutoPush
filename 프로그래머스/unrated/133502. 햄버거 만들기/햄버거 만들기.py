cbea353ca2cfb2bdd7339455394281f40fdb10d8
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-13 16:52:50 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -8,12 +8,9 @@ def solution(ingredient):
     for n in ingredient:
         tmp.append(n)
         
-        if len(tmp) >= 4 and tmp[-1] == 1:
-            if tmp[-2] == 3 and tmp[-3] == 2 and tmp[-4] == 1:
+        if tmp[-4:] == [1,2,3,1]:
+            answer += 1
+            for i in range(4):
                 tmp.pop()
-                tmp.pop()
-                tmp.pop()
-                tmp.pop()
-                answer += 1
     
     return answer

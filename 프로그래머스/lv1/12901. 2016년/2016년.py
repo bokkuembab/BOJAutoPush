232ed52d288c35f61c000a920be4db00d2e75582
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-15 18:13:11 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -2,9 +2,6 @@ def solution(a, b):
     mdays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     days = ['THU','FRI','SAT', 'SUN','MON','TUE','WED']
     
-    total_days = 0
-    for i in range(a - 1):
-        total_days += mdays[i]
-    total_days += b
+    answer = days[( sum(mdays[:a-1]) + b ) % 7]
     
-    return days[total_days % 7]
+    return answer

e6a714c878e03b6cf0726cd9dff44abc5aa7a68d
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-17 18:54:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,10 @@
+def solution(num):
+    if num == 1:
+        return 0
+    
+    for i in range(1, 501):
+        num = num / 2 if num % 2 == 0 else num * 3 + 1
+        if num == 1:
+            return i
+
+    return -1

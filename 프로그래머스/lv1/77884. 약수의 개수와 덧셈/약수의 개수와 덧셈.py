a378b207b99f547c4aaadf0842bc377c24b8895f
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-17 22:03:56 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,14 +1,10 @@
-def times(n):
-    t = 0
-    for i in range(1, int(n ** 0.5) + 1):
-        if n % i == 0:
-            t += 2
-            
-    if int(n ** 0.5) == n ** 0.5:
-        t -= 1
-        
-    return t % 2 == 0
-
 def solution(left, right):
+    answer = 0
     
-    return sum(x if times(x) else -x for x in range(left, right + 1))
+    for n in range(left, right + 1):
+        if int(n ** 0.5) == n ** 0.5:
+            answer -= n
+        else:
+            answer += n
+    
+    return answer

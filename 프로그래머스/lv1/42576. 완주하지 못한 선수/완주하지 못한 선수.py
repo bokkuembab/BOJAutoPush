9bee896ba44049373338d1ba1e4cc6bd94276232
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-22 17:31:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.30 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,8 @@
+from collections import Counter
+
 def solution(participant, completion):
-    check = {}
     
-    for p in participant:
-        if p not in check:
-            check[p] = 1
-        else:
-            check[p] += 1
-        
-    for c in completion:
-        check[c] -= 1
-            
-    for p in check:
-        if check[p] != 0:
-            print(check[p])
-            return p
+    ans = Counter(participant) - Counter(completion)
+    ans = list(ans.keys())[0]
+    
+    return ans

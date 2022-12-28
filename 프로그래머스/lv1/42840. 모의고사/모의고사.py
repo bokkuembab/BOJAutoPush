c6730f703a4a7acf89a2a7b7ff13b4ec71407f05
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-28 15:49:02 +0900

    [level 1] Title: 모의고사, Time: 3.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,18 +1,16 @@
 def solution(answers):
-    ans = []
     
     students = [[1,2,3,4,5], 
                 [2,1,2,3,2,4,2,5], 
                 [3,3,1,1,2,2,4,4,5,5]]
+    ans = [0, 0, 0]
     
     for i in range(len(students)):
-        st = [students[i][j % len(students[i])] for j in range(len(answers))]
-        
-        for i in range(len(st)):
-            st[i] = st[i] - answers[i]
-        ans.append(st.count(0))
-    print(max(ans))
-    
-    ans = [i + 1 for i in range(len(ans)) if ans[i] >= max(ans)]
+        cycle = len(students[i])
+        for j in range(len(answers)):
+            if students[i][j % cycle] == answers[j]:
+                ans[i] += 1
+
+    ans = [i + 1 for i in range(len(ans)) if ans[i] == max(ans)]
             
     return ans

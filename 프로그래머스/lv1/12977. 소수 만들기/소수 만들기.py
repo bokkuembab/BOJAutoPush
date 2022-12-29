936866dcaff7ede4ff87a013a57e838594f20365
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-29 19:29:02 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,15 +1,13 @@
 from itertools import combinations
 
-def is_prime(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return 0
-    return 1
-
 def solution(nums):
     answer = 0
     
     for s in list(sum(c) for c in combinations(nums, 3)):
-        answer += is_prime(s)
+        for i in range(2, int(s ** 0.5) + 1):
+            if s % i == 0:
+                break
+        else:
+            answer += 1
 
     return answer

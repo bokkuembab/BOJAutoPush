f63e5fc965df7f898b9d55e2abc14808be94c5d6
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2022-12-30 18:58:23 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,8 +1,8 @@
 def solution(food):
     answer = ''
     
-    for i in range(len(food)):
-        answer += '%d'%i * (food[i] // 2)
+    for i,n in enumerate(food):
+        answer += str(i) * (n // 2)
         
     answer = answer + '0' + answer[::-1]
     

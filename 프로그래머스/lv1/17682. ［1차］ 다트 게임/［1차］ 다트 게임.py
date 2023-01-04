a4ccd279f5f1a11ac3ad48caf2899d026116e7de
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-01-05 02:22:59 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,39 +1,22 @@
 def solution(dartResult):
     
     ans = []
-    scores = [str(i) for i in range(0, 10)]
+    dartResult = dartResult.replace('10', '-')
+    scores = ['10' if i == '-' else i for i in dartResult]
+    options = ['S', 'D', 'T']
     
-    # 3회의 dart 나누기
-    for i in range(2, len(dartResult)):
-        if dartResult[i] == '1' and dartResult[i:i+2] == '10':
-            continue
-        
-        if dartResult[i] == '0' and dartResult[i-1:i+1] == '10':
-            ans.append(dartResult[:i-1])
-        elif dartResult[i] in scores:
-            ans.append(dartResult[:i])
-    
-    ans.append(dartResult.replace(ans[1], ""))
-    ans[1] = ans[1].replace(ans[0], "")
-    
-    for time, dart in enumerate(ans):
-        print(time, dart)
-        if dart[1] == '0':
-            ans[time], i = int(dart[:2]), 2
+    i = -1
+    for o in scores:
+        if o in options:
+            ans[i] = ans[i] ** (options.index(o) + 1)
+        elif o == '*':
+            ans[i] *= 2
+            if i != 0:
+                ans[i-1] *= 2
+        elif o == '#':
+            ans[i] = ans[i] * (-1)
         else:
-            ans[time], i = int(dart[0]), 1
-            
-        
-        if dart[i:i+1] == 'D':
-            ans[time] = ans[time] ** 2
-        elif dart[i:i+1] == 'T':
-            ans[time] = ans[time] ** 3
-            
-        if dart[i+1:i+2] == '*':
-            ans[time-1] *= 2
-            ans[time] *= 2
-        elif dart[i+1:i+2] == '#':
-            ans[time] = -ans[time]
-    
+            ans.append(int(o))
+            i += 1
     
     return sum(ans)

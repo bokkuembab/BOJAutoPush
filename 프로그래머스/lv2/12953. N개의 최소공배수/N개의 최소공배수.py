bacec91a406afbdd3b39cffa015b1fe38159be52
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-01-06 21:58:01 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,4 +1,3 @@
-# 최대공약수 찾는 재귀 함수: 유클리드 호제법
 def gcd(x, y):
     if y == 0:
         return x
@@ -10,8 +9,6 @@ def solution(arr):
     
     for n in arr:
         g = gcd(lcm, n)
-        # 최소공배수: 두 수의 곱 / 최대공약수
         lcm = int(n * lcm / g)
-        print(lcm)
         
     return lcm

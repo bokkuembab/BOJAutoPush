0e5f4d25f9422331e98133cd89f06d181c730ea1
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-02-03 14:05:14 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,7 +1,8 @@
+# N: 수열의 길이, A: 수열 리스트, X: 기준값
+
 N, X = map(int, input().split())
+data = list(map(int, input().split()))
 
-A = list(map(int, input().split()))
-
-for i in range(0,N):
-    if A[i] < X:
-        print(A[i], end=' ')
+for n in data:
+    if n < X:
+        print(n, end=' ')

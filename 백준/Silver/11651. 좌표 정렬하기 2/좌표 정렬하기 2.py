7ef715b05a0497292d36ba7b466e0b419097c4ea
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-02-12 15:39:02 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 384 ms, Memory: 54084 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,13 @@
+# dot_n: 점의 개수, dots: 점들을 저장하는 리스트
+import sys
+input = sys.stdin.readline
+
+dot_n = int(input())
+dots = []
+
+for _ in range(dot_n):
+    dots.append(tuple(map(int, input().split())))   # 점 하나씩 튜플 형태로 입력받아 리스트에 넣기
+
+dots.sort(key=lambda d: [d[1], d[0]])    # y, x 좌표 순서대로 정렬
+for x, y in dots:
+    print(x, y)   # 출력

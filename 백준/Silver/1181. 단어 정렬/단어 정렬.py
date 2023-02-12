08f8e58d3348e16558d110f0ebc72b2574fda141
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-02-12 16:05:09 +0900

    [Silver V] Title: 단어 정렬, Time: 100 ms, Memory: 37556 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,9 +1,10 @@
 # word_n: 단어의 개수, word_li: 입력받은 단어 저장 집합(set)
+import sys
 
-word_n = int(input())
+word_n = int(sys.stdin.readline())
 word_li = set()    # 중복을 제거하기 위해 집합 자료형으로 생성
 for _ in range(word_n):
-    word_li.add(input())
+    word_li.add(sys.stdin.readline().strip())
     
 word_li = sorted(word_li, key=lambda w: [len(w), w])    # 길이순, 사전순 기준으로 정렬
 

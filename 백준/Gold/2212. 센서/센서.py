1122a2e9bcd891b38a16fa62a3de27e5c7cb4be6
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-02-14 03:08:39 +0900

    [Gold V] Title: 센서, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -1,15 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-k = int(input())
-sensor = list(map(int,input().split()))
-sensor.sort()
+sen = int(input())    # sen_num: 센서의 개수
+net = int(input())    # net_num: 집중국의 개수
+sen_list = list(map(int, input().split()))    # net_list: 센서 좌표 리스트
+dist_list =[]    # 센서간의 거리 저장 리스트
 
-array = []
-for i in range(0,n-1):
-    array.append(sensor[i+1] - sensor[i])
+sen_list.sort()    # 센서 위치 순서대로 정렬
 
-array.sort()
-
-print(sum(array[:n-k]))
+for i in range(0, sen - 1):   # 센서 간의 거리 계산
+    dist_list.append(sen_list[i + 1] - sen_list[i])
+    
+dist_list.sort()    # 센서 간 거리 리스트 정렬
+print(sum(dist_list[:sen - net]))    # 가장 거리가 먼 구간부터 센서 배치

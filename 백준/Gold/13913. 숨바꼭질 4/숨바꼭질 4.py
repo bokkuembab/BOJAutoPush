6dc5d739cf6bf9b2e1ae978d230a8081e9838d47
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-03-29 21:15:08 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 212 ms, Memory: 40736 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+start, end = map(int, input().split())    # now: 현재 위치, target: 목표 위치
+
+dist = [10 ** 6] * (10 ** 5 + 1)    # 최단거리는 최대값으로 초기화
+path = [-100] * (10 ** 5 + 1)    # 경로를 저장할 리스트
+
+# 시작 지점 초기화
+q = deque([(start, 0)])
+dist[start] = 0    # 현재 위치로의 이동 횟수는 0
+
+while q:
+    now, cnt = q.popleft()
+    
+    if now == end:
+        break
+    
+    for move in [now * 2, now + 1, now - 1]:
+        if 0 <= move <= 10 ** 5:    # 범위를 넘지 않고,
+            if cnt + 1 < dist[move]:    # 최단 횟수라면,
+                dist[move] = cnt + 1    # dist 테이블 갱신하고,
+                q.append((move, cnt + 1))    # q에 넣어주고,
+                path[move] = now    # path 테이블에 경로 추가하기
+                
+print(dist[end])     # 최단 이동 횟수 출력
+
+# 경로 타고 넣어주기
+i = end
+res = deque()
+while i != start:
+    res.appendleft(i)
+    i = path[i]
+res.appendleft(start)
+print(*res)

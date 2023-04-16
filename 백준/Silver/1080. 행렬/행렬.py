5e9c96f6d5df5f58ab3f9a2d8faa23fc51a7b0ba
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-04-16 20:56:02 +0900

    [Silver I] Title: 행렬, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.py
@@ -0,0 +1,48 @@
+# 행렬 A->B로의 변환 횟수의 최소값 구하기 (변환 불가능 -> -1 출력)
+# 3*3 크기의 부분 행렬의 모든 원소를 토글함
+# 3*3 크기보다 작은데, 다른 요소가 있다 -> 변경 불가
+# 행, 열의 길이는 50이하의 자연수 -> 단순 반복 가능
+
+import sys
+input = sys.stdin.readline
+
+# 입력 받기
+n, m = map(int, input().split())    # 행렬의 크기 (50이하의 자연수)
+ali = [list(map(int, input().rstrip())) for _ in range(n)]    # 행렬 A
+bli = [list(map(int, input().rstrip())) for _ in range(n)]    # 행렬 B
+
+
+# 초기화
+ans = 0    # 토글 횟수를 0으로 초기화
+
+# 3*3 행렬 뒤집는 함수 생성
+def flip_33(sr, sc):
+    for row in range(sr, sr + 3):
+        for col in range(sc, sc + 3):
+            ali[row][col] = 1 - ali[row][col]
+
+# 3*3 크기보다 작은데, 다른 요소가 있는지 먼저 고려
+# 행, 열 중 하나라도 크기가 3보다 작으면 토글 불가
+if (len(ali) < n) or (len(ali[0]) < m):    
+    for row in range(len(ali)):
+        if ali[row] != bli[row]:
+            ans = -1
+            break
+# 3*3 크기로 한 칸씩 옮겨가며 확인
+else:
+    for row in range(n - 2):    # 행 방향 이동 가능 횟수
+        for col in range(m - 2):    # 열 방향 이동 가능 횟수
+            if ali[row][col] != bli[row][col]:    # 3*3의 첫번째 요소가 같지 않다면,
+                flip_33(row, col)    # 3*3 부분 행렬 뒤집고,
+                ans += 1    # 횟수 늘려주기
+                
+            if ali == bli:
+                break
+        if ali == bli:
+            break   
+
+# 결과 출력
+if ali == bli:    # 전체를 돌면서 토글했을 때 같다면
+    print(ans)    # 횟수 출력
+else:    # 다르다면
+    print(-1)    # -1 출력

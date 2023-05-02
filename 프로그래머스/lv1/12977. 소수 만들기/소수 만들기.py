9bcf08710e438c83e95437b4dac0b3b752715cb6
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-05-02 20:13:53 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,13 +1,31 @@
-from itertools import combinations
+# combination 함수 구현
+def combination(arr, n):
+    
+    res = []  # 결과를 담을 리스트
+    
+    if n == 0:   # 0개를 고른다면, 빈 행렬 출력
+        return [[]]
+    
+    for i in range(len(arr) - n + 1):  # 더 이상 n개를 고를 수 없을 때까지 순회
+        ele = arr[i]   # 시작 요소 넣기
+        for rest in combination(arr[i + 1:], n - 1):
+            res.append([ele] + rest)
+            
+    return res
+
+# 소수 확인 함수 구현
+def is_prime(n):
+    if n == 1:
+        return 0
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return 0
+    return 1
 
 def solution(nums):
-    answer = 0
+    ans = 0
     
-    for s in list(sum(c) for c in combinations(nums, 3)):
-        for i in range(2, int(s ** 0.5) + 1):
-            if s % i == 0:
-                break
-        else:
-            answer += 1
-
-    return answer
+    for snums in [sum(c) for c in combination(nums, 3)]:
+        ans += is_prime(snums)
+        
+    return ans

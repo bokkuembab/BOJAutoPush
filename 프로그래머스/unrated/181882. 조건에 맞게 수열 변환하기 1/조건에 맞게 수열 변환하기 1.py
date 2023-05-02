2aade3b42399018d758d2ebecd989fcfab6a4b2c
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-05-02 11:32:45 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 15.51 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -2,11 +2,8 @@ def solution(arr):
     answer = []
     
     for n in arr:
-        if n >= 50 and n % 2 == 0:
-            answer.append(n / 2)
-        elif n < 50 and n % 2 != 0:
-            answer.append(n * 2)
-        else:
-            answer.append(n)
+        if n >= 50 and not n % 2: answer.append(n / 2)
+        elif n < 50 and n % 2: answer.append(n * 2)
+        else: answer.append(n)
     
     return answer

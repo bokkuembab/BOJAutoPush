b90bfb12ad1dd7f30142ca5a10756513a1a1af8f
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-05-02 11:45:37 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,7 +1,4 @@
 def solution(num_list):
-    id = -1
     for i, n in enumerate(num_list):
-        if n < 0:
-            id = i
-            break
-    return -1 if id < 0 else id
+        if n < 0: return i
+    return -1

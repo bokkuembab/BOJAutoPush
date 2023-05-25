bc7e94f74d11d24f95dadbb77da344dcc3b8131a
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-05-26 01:38:14 +0900

    [Gold IV] Title: 테트로미노, Time: 5352 ms, Memory: 39028 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,64 +1,63 @@
+# 종이의 범위를 넘지 않으면서, 연속적인 칸 4개를 방문하면 됨.
+# but, ㅏㅗㅜㅓ 모양은 다시 돌아가서 방문해야 함 (-> 이를 고려해줘야 함)
+
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
-# 상, 하, 좌, 우
-move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+row, col = map(int, input().split())
+note = [list(map(int, input().split())) for _ in range(row)]
 
-# INPUT
-N, M = map(int, input().split())
-board = [list(map(int,input().split())) for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
+move = [(1, 0), (-1, 0), (0, 1), (0, -1)]    # 이동 가능한 경우의 수 (상하좌우)
+res = 0    # 최대합
+visited = [[False] * col for _ in range(row)]
 
-# 최대값 변수
-maxValue = 0
-
-# ㅗ, ㅜ, ㅓ, ㅏ 제외한 모양들 최대값 계산
-def dfs(i, j, dsum, cnt):
-    global maxValue
-    # 모양 완성되었을 때 최대값 계산
-    if cnt == 4:
-        maxValue = max(maxValue, dsum)
+# ㅗ ㅜ ㅏ ㅓ 제외했을 때, 최대합 계산
+def search_note(r, c, cur, cnt): 
+    global res
+    if cnt == 4:    # 4칸을 다 방문했을 때
+        res = max(cur, res)    # 현재까지의 합과 기존 값 중 큰 값으로 결과 변경
         return
-
-    # 상, 하, 좌, 우로 이동
-    for n in range(4):
-        ni = i+move[n][0]
-        nj = j+move[n][1]
-        if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj]:
-            # 방문 표시 및 제거
-            visited[ni][nj] = True
-            dfs(ni, nj, dsum + board[ni][nj], cnt+1)
-            visited[ni][nj] = False
-
-
-# ㅗ, ㅜ, ㅓ, ㅏ 모양의 최대값 계산
-def exce(i, j):
-    global maxValue
+    
+    for i, j in move:
+        mr = r + i
+        mc = c + j
+        
+        if 0 <= mr < row and 0 <= mc < col:    # 이동한 곳이 범위를 넘어가지 않고, 
+            if not visited[mr][mc]:    # 방문한 적이 없다면
+                # 방문 표시 및 순회 후, 다시 표시 제거
+                visited[mr][mc] = True
+                search_note(mr, mc, cur + note[mr][mc], cnt + 1)
+                visited[mr][mc] = False
+    
+# ㅗ ㅜ ㅏ ㅓ 만 고려했을 때, 최대합 계산            
+def search_note_rest(r, c):
+    global res
     for n in range(4):
         # 초기값은 시작지점의 값으로 지정
-        tmp = board[i][j]
+        tmp = note[r][c]
         for k in range(3):
             # move 배열의 요소를 3개씩 사용할 수 있도록 인덱스 계산
             # 012, 123, 230, 301
             t = (n+k)%4
-            ni = i+move[t][0]
-            nj = j+move[t][1]
+            ni = r+move[t][0]
+            nj = c+move[t][1]
 
-            if not (0 <= ni < N and 0 <= nj < M):
+            if not (0 <= ni < row and 0 <= nj < col):
                 tmp = 0
                 break
-            tmp += board[ni][nj]
+            tmp += note[ni][nj]
         # 최대값 계산
-        maxValue = max(maxValue, tmp)
-
-
-for i in range(N):
-    for j in range(M):
-        # 시작점 visited 표시
-        visited[i][j] = True
-        dfs(i, j, board[i][j], 1)
-        visited[i][j] = False
-
-        exce(i, j)
-
-print(maxValue)
+        res = max(res, tmp)
+        
+    
+                
+for r in range(row):
+    for c in range(col):
+        visited[r][c] = True    # 시작점 방문처리
+        search_note(r, c, note[r][c], 1)
+        visited[r][c] = False    # 시작점 방문처리 해제
+        
+        search_note_rest(r, c)
+        
+print(res)

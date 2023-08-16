bec0387337b252eeb7418a61b199395cf06876f2
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-08-16 11:10:50 +0900

    [level 2] Title: 타겟 넘버, Time: 2.83 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,34 @@
+def solution(numbers, target):
+    ans = 0
+    
+    # +/-로 가지가 계속 뻗어나감, 어차피 모든 경우의 수 확인해야 함
+    # -> bfs/dfs 둘 다 상관 없을 듯
+    # now_idx: 현재 트리 깊이, now_sum: 현재까지의 합
+    def make_target(now_idx, now_sum):
+        nonlocal ans
+        
+        # 숫자의 끝에 도달했으면, 종료
+        if now_idx == len(numbers):
+            if now_sum == target:    # 타겟 숫자를 만들었으면, 정답 추가
+                ans += 1
+            return 0
+        
+        # 모든 경우의 수(+, -) 탐색
+        make_target(now_idx + 1, now_sum + numbers[now_idx])
+        make_target(now_idx + 1, now_sum - numbers[now_idx])
+    
+    make_target(0, 0)
+    
+    return ans
+
+
+
+
+
+
+
+
+
+
+
+

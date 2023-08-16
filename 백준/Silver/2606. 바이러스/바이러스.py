79dfdf3c85d8f7a51a710033ec80795f5051af28
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-08-16 09:58:39 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,31 +1,33 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-# 입력 받기
-n_com = int(input())    # 컴퓨터의 수
-n_net = int(input())    # 컴퓨터끼리 연결된 네트워크의 수
-coms = [[] for _ in range(n_com + 1)]    # 컴퓨터 간의 연결 정보 저장 리스트
-for _ in range(n_net):
-    s, e = map(int, input().split())
-    coms[s].append(e)
-    coms[e].append(s)
-    
-# 초기 값 설정
-cnt = 0    # 시작 컴퓨터를 통해 감염되는 컴퓨터의 수
-start = 1    # 시작 컴퓨터는 1
-visited = [False] * (n_com + 1)    # 방문 여부 리스트
-q = deque([start])
-visited[start] = True    # 시작 컴퓨터 방문처리
+# 입력받기
+num_pc = int(input())    # 컴퓨터의 수
+num_node = int(input())    # 연결된 컴퓨터 쌍의 수
+# 연결쌍의 정보리스트 (컴퓨터 번호는 1부터)
+couple_info = [[] for _ in range(num_pc + 1)]    
+for _ in range(num_node):
+    pc1, pc2 = map(int, input().split())
+    couple_info[pc1].append(pc2)
+    couple_info[pc2].append(pc1)
 
-while q:    # 큐가 빌 때까지 반복
-    c = q.popleft()
-    
-    # 현재 컴퓨터와 연결된 컴퓨터 모두 확인
-    for i in coms[c]:
-        if not visited[i]:    # 방문한 적 없다면,
-            visited[i] = True    # 방문처리,
-            q.append(i)    # q에 추가,
-            cnt += 1    # 감염 컴퓨터 늘려주기
-            
-print(cnt)    # 결과 출력
+# 바이러스에 걸린 pc의 수를 계산하는 함수
+# dfs(재귀)로 구현
+def search_virus_pc(now, visited):
+    global num_virus_pc
+
+    # 현재 노드 방문 처리
+    visited[now] = True
+    num_virus_pc += 1
+
+    # 연결된 모든 노드 확인
+    for i in couple_info[now]:
+        if not visited[i]:    # 방문한 적이 없다면, 방문하기
+            search_virus_pc(i, visited)
+
+num_virus_pc = 0    # 바이러스에 걸린 pc의 수
+visited = [False] * (num_pc + 1)    # 방문 여부 리스트
+
+# 바이러스 pc 확인 및 결과출력
+search_virus_pc(1, visited)
+print(num_virus_pc - 1)    # 1번 컴퓨터 제외

ed931d3da1bcbb0c22ea542d5f753eb61a76ac21
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-09-11 20:59:26 +0900

    [Silver II] Title: 용돈 관리, Time: 408 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -5,13 +5,18 @@ input = sys.stdin.readline
 n, target = map(int, input().split())
 # 매일 이용할 금액 리스트
 days = list(int(input()) for _ in range(n))
-ans = sum(days)    # 최대 인출 금액으로 초기화
+# 최대 인출 금액(전체 일수에서 필요한 금액 총합)으로 초기화
+ans = sum(days)    
 
-left, right = min(days), ans
+# 매일 필요한 금액보다는 인출 금액이 커야하므로
+# 최소 금액을 가장 작은 금액으로 설정
+left, right = max(days), ans
 while left <= right:
     mid = (left + right) // 2
+    # 현재 인출한 금액, 횟수(처음에 1회 인출하고 시작)
+    now, cnt = mid, 1    
 
-    now, cnt = mid, 1    # 현재 인출한 금액, 횟수
+    # 전체 일수 돌면서 확인
     for mon in days:
 
         if now < mon:    # 갖고 있는 금액 적으면, 더 인출
@@ -20,7 +25,8 @@ while left <= right:
         else:    # 충분히 크면, 현재에서 빼주기
             now -= mon
 
-    if cnt > target or mid < max(days):
+    # 인출 횟수가 목표보다 클 때, 인출값 키워주기
+    if cnt > target:
         left = mid + 1
     else:
         ans = min(ans, mid)

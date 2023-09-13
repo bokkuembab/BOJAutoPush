ffc8434e37b94c67f93ac61ec160a25a8dda69f7
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-09-13 11:41:46 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 3832 ms, Memory: 155568 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -1,22 +1,32 @@
-N = int(input())
-A = list(map(int, input().split()))
-LIS = [A[0]]
+# LIS + 이분탐색
+# LIS 리스트를 만드는 과정에서 각 값들을 찾을 때, 이분탐색 활용
 
-for n in A[1:]:
-    if LIS[-1] < n:
-        LIS.append(n)
-    else: 
-        left = 0
-        right = len(LIS)-1
-        
-        while left < right:
-            mid = (left + right) // 2
+import sys
+input = sys.stdin.readline
 
-            if LIS[mid] < n:
-                left = mid + 1
-            else:
-                right = mid
+n = int(input())    # 수열의 길이
+nlist = list(map(int, input().split()))    # 수열
+lis = [nlist[0]]    # 가장 긴 증가하는 부분 수열
 
-        LIS[right] = n
+for i in nlist[1:]:
 
-print(len(LIS))
+    # lis 리스트의 가장 큰 수보다 크다면, 그대로 넣어주기
+    if i > lis[-1]:    
+        lis.append(i)
+        continue
+    
+    # 아니라면, lis 리스트에서 i가 들어갈 자리 찾음 (이분탐색)
+    # lis 리스트는 오름차순으로 정렬되어 있기 때문에 이분탐색 가능
+    left, right = 0, len(lis)
+    while left < right:
+        mid = (left + right) // 2
+
+        if lis[mid] < i:
+            left = mid + 1
+        else:
+            right = mid
+
+    # 찾은 위치에 해당 값 갱신
+    lis[right] = i
+
+print(len(lis))

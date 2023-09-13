ed95ac188ba064c85fdb7dea5bc0421defd6d724
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-09-13 11:50:43 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 2152 ms, Memory: 155576 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -4,29 +4,35 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())    # 수열의 길이
+nl = int(input())    # 수열의 길이
 nlist = list(map(int, input().split()))    # 수열
 lis = [nlist[0]]    # 가장 긴 증가하는 부분 수열
 
-for i in nlist[1:]:
-
-    # lis 리스트의 가장 큰 수보다 크다면, 그대로 넣어주기
-    if i > lis[-1]:    
-        lis.append(i)
-        continue
-    
-    # 아니라면, lis 리스트에서 i가 들어갈 자리 찾음 (이분탐색)
+def binary_search(target):
     # lis 리스트는 오름차순으로 정렬되어 있기 때문에 이분탐색 가능
     left, right = 0, len(lis)
     while left < right:
         mid = (left + right) // 2
 
-        if lis[mid] < i:
+        if lis[mid] == target:    # 해당 값 찾으면, 종료
+            return mid
+        elif lis[mid] < target:
             left = mid + 1
         else:
             right = mid
 
-    # 찾은 위치에 해당 값 갱신
-    lis[right] = i
+    # 못 찾고 끝났을 때, 해당 값이 right보다 작음 
+    # -> 해당 값으로 갱신
+    return right
+
+for n in nlist[1:]:
+
+    # lis 리스트의 가장 큰 수보다 크다면, 그대로 넣어주기
+    if n > lis[-1]:    
+        lis.append(n)
+        continue
+    
+    # 아니라면, lis 리스트에서 i가 들어갈 자리 찾음 (이분탐색)
+    lis[binary_search(n)] = n
 
 print(len(lis))

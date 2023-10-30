4eed49517ca3535cea2a16c19aa3aaf32f948303
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-10-30 11:27:51 +0900

    [level 2] Title: 괄호 변환, Time: 0.65 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,24 +1,15 @@
 # 올바른 괄호인지 확인하는 함수
 def is_correct(p):
-    stack = []
+    cnt = 0
     for i in p:
-        # 스택이 비어있고, 여는 괄호면 추가
-        if not stack:    
-            if i == ')':    # 닫는 괄호면, 성립 X -> 종료
-                return False
-            stack.append(i)
-            continue
-            
-        # 괄호가 닫혔으면, 스택에서 제거
-        if stack[-1] != i:    
-            stack.pop()
-        else:
-            stack.append(i)
-    
-    if stack:
-        return False
-    else:
-        return True
+        if i == '(':    # 여는 괄호 +1
+            cnt += 1
+        else:    # 닫는 괄호 -1
+            cnt -= 1
+        # 닫는 괄호의 수가 더 많으면, 성립 X -> 종료
+        if cnt < 0:
+            return False
+    return True
 
 # 올바른 괄호로 변환하는 함수
 def make_correct(p):

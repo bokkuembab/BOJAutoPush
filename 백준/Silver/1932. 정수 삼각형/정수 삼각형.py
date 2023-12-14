2c4f8cc66b6a2ab5fcd7e1209f9d080837e0e915
bokkuembab <88229105+bokkuembab@users.noreply.github.com>
2023-12-14 22:47:00 +0900

    [Silver I] Title: 정수 삼각형, Time: 144 ms, Memory: 40628 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -2,21 +2,18 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())    # 정수 삼각형의 크기
-triangle = [list(map(int, input().split())) for _ in range(n)]    # 정수 삼각형의 정보
-dp = []    # 해당 위치의 최대 합
-for i in range(1, n + 1):
-    dp.append([0] * i)
-dp[0] = triangle[0]    # 시작 위치 초기화
+tri = [list(map(int, input().split())) for _ in range(n)]    # 정수 삼각형의 정보
+dp = [[0] * i for i in range(1, n + 1)]    # 해당 위치의 최대 합
+dp[0] = tri[0]    # 시작 위치 초기화
     
 # 순차적으로 탐색
-for r in range(1, n):    # (2 ~ n) 탐색
-    for c in range(r + 1):
-        if c == 0:    # 왼쪽 변의 값이라면,
-            dp[r][c] = triangle[r][c] + dp[r - 1][c]    # 바로 위의 값
-        elif c == r:    # 오른쪽 변의 값이라면,
-            dp[r][c] = triangle[r][c] + dp[r - 1][c - 1]    # 바로 위의 값
+for d in range(1, n):    # (2 ~ n) 탐색
+    for i in range(d + 1):
+        if i == 0:    # 왼쪽 변의 값이라면,
+            dp[d][i] = tri[d][i] + dp[d - 1][i]    # 바로 위의 값
+        elif i == d:    # 오른쪽 변의 값이라면,
+            dp[d][i] = tri[d][i] + dp[d - 1][i - 1]    # 바로 위의 값
         else:     # 중앙의 값이라면,
-            dp[r][c] = triangle[r][c] + max(dp[r - 1][c - 1:c + 1])    # (r - 1)의 (c - 1, c) 탐색
+            dp[d][i] = tri[d][i] + max(dp[d - 1][i - 1:i + 1])    # (d - 1)의 (i - 1, i) 탐색
             
 print(max(dp[n - 1]))
-            
